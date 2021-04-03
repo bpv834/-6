9eb5a7d726e7a48e41b39d5bbec05ee832b768b7
bpv834 <81566093+bpv834@users.noreply.github.com>
2021-04-03 16:27:08 +0900

    초기 커밋.

---
+++ ConsoleApplication30/ConsoleApplication30.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <stdio.h>
+
+int main(void)
+{
+    int num, i, cnt = 0;
+    int Oddsum = 0;
+    double avg = 0;
+
+    printf("정수를입력하시오 ");
+    scanf_s("%d", &num);
+
+
+
+    for (i = 1; i <= num; i += 2)
+    {
+        Oddsum += i;
+        cnt++;
+    }
+
+
+    printf("1부터%d까지의홀수합은%d입니다\n", num, Oddsum);
+    printf("평균은%d:", Oddsum / cnt);
+
+}
+
